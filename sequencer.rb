a05268a430247932e6ab106d87f9c20c06386361
Yiannis Moses <yiannis.moses@Centro-103794.centro.net>
2016-08-04 14:12:23 -0500

    updating sequencer

--- sequencer.rb
+++ sequencer.rb
@@ -1,14 +1,19 @@
 def sequencer(list_of_integers)
   sequenced_string = ''
   list_of_integers.each_with_index do |integer, index|
-    sequence_start = index == 0
     next_integer = list_of_integers[index + 1]
     sequenced_string += integer.to_s
     unless next_integer.nil?
       next_integer - integer == 1 ? sequenced_string += "-" : sequenced_string += ","
-      sequenced_string.slice!(sequenced_string.length - integer.to_s.length - 1, sequenced_string.length) if !sequence_start && sequenced_string.end_with?("-#{integer}-")
+      trim_mid_range_integers(sequenced_string, integer)
     end
   end
   sequenced_string
 end
 
+def trim_mid_range_integers(sequenced_string, integer)
+  if sequenced_string.end_with?("-#{integer}-")
+    mid_range_index = sequenced_string.length - integer.to_s.length - 1
+    sequenced_string.slice!(mid_range_index, sequenced_string.length)
+  end
+end
